6f3870e6e0c5ce85b32c18ed3883c32b660d407c
Superschnapp <102432618+Superschnapp@users.noreply.github.com>
2022-12-13 09:48:38 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Ni":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. 7 7 7 7 7 7 7 7 7 8 7 7 7 7 . 
+7 2 2 2 2 7 7 7 7 8 8 8 8 7 7 7 
+. 7 7 7 7 7 7 7 8 8 2 2 8 7 7 . 
+. . . . 7 7 7 7 7 8 2 2 5 . 5 . 
+. . . . . 7 7 7 2 2 2 f 4 5 5 . 
+. . . . 2 f 2 2 2 2 f f 4 2 4 5 
+. . . . . 7 2 2 2 7 f f 4 2 4 5 
+. . . . 2 f 2 2 2 7 7 f 2 4 2 4 
+. . . . . 7 7 7 7 7 7 7 2 2 5 5 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
